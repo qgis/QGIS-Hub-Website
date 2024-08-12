882bf398edfd34388f510743cdf8576f8e338f5a
Lova ANDRIARIMALALA <43842786+Xpirix@users.noreply.github.com>
2024-08-12 12:36:20 +0300

    Fix navigation bar, redirect root url to /styles

--- qgis-app/urls.py
+++ qgis-app/urls.py
@@ -8,6 +8,7 @@ from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from django.shortcuts import redirect
 
 admin.autodiscover()
 
@@ -77,6 +78,11 @@ urlpatterns += [
         name="schema-swagger-ui",
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+]
+
+# Redirect the root url to the style page
+urlpatterns += [
+    path('', lambda request: redirect('/styles/?order_by=-upload_date&&is_gallery=true', permanent=True)),
 ]
 
 
