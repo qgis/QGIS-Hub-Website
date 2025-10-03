28de1be37b5b406ad9c22ed0a08ae83bef95bfdf
Xpirix <xpirixii@gmail.com>
2025-10-03 11:12:33 +0300

    Refactor repository URL patterns and update FetchAllRepo view to fetch updates from all remote repositories

--- qgis-app/contributors/urls.py
+++ qgis-app/contributors/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import CommitCountList, CommitCountByAuthor
+
+from .views import CommitCountByAuthor, FetchAllRepo
 
 urlpatterns = [
-    path("<str:repo>/commit-counts/", CommitCountList.as_view(), name="commit-counts"),
-    path("<str:repo>/commit-counts/<str:author>/", CommitCountByAuthor.as_view(), name="commit-count-by-author"),
+    path(
+        "<str:repo>/commit-counts/<str:author>/",
+        CommitCountByAuthor.as_view(),
+        name="commit-count-by-author",
+    ),
+    path("fetch-all-repos/", FetchAllRepo.as_view(), name="fetch-all-repos"),
 ]
